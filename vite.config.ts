40727a5facbfec265264383dc734e6bbe6ee48bc
Evandro Calado <evandrocalado07@gmail.com>
2024-11-02 13:43:28 -0300

    fix: declaration type file on build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true, tsconfigPath: './tsconfig.app.json' }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
