c3c04da27f7ca7e8990edfc0be56625acbfd5cde
Evandro Calado <evandrocalado07@gmail.com>
2024-11-26 10:55:39 -0300

    build: build storybook

--- storybook-static/assets/CardTitle.stories-DUMcm-NR.js
+++
@@ -1,5 +0,0 @@
-import{C as i}from"./CardTitle-GDIO2MZR.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./index-D2SpNwgl.js";const u={title:"Components/Card/CardTitle",component:i,args:{children:"Card Title"},parameters:{layout:"centered"}},r={args:{children:"Card Title"}},e={};var t,o,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: 'Card Title'
-  }
-}`,...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var s,c,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(n=(c=e.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const C=["WithIcon","WithoutIcon"];export{r as WithIcon,e as WithoutIcon,C as __namedExportsOrder,u as default};
---
+++ storybook-static/assets/CardTitle.stories-DXCuP1bB.js
@@ -0,0 +1,5 @@
+import{C as o}from"./CardTitle-GDIO2MZR.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./index-D2SpNwgl.js";const c={title:"Components/Card/CardTitle",component:o,args:{children:"Card Title"},parameters:{layout:"centered"}},r={args:{children:"Card Title"}};var e,t,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    children: 'Card Title'
+  }
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const l=["Default"];export{r as Default,l as __namedExportsOrder,c as default};
